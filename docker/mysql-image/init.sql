c36283f8cdabe001e0c24df9263ace197b47d246
Neiman, Victor <victor.neiman@accenture.com>
2018-03-21 10:19:39 +0100

    Setting col lenght to 20 for content

--- docker/mysql-image/init.sql
+++ docker/mysql-image/init.sql
@@ -2,5 +2,5 @@ create database cucumber;
 create user 'springuser'@'asynch-request-reader' identified by 'ThePassword';
 grant all on *.* to 'springuser'@'%' identified by 'ThePassword';
 create table  cucumber.request (id INT NOT NULL AUTO_INCREMENT,
-                                content VARCHAR(255),
+                                content VARCHAR(20),
                                 PRIMARY KEY (id));
