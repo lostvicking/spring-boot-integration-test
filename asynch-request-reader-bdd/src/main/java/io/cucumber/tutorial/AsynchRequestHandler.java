96a1de27718225a08795aae79ceceee8fa930371
Neiman, Victor <victor.neiman@accenture.com>
2018-10-16 15:50:10 +0200

    Vic: removed ex

--- asynch-request-reader-bdd/src/main/java/io/cucumber/tutorial/AsynchRequestHandler.java
+++ asynch-request-reader-bdd/src/main/java/io/cucumber/tutorial/AsynchRequestHandler.java
@@ -18,13 +18,11 @@ public class AsynchRequestHandler {
 
     @RabbitHandler
     public void receive(String in) {
-        throw new RuntimeException("Woops");
-        /*
         System.out.println(" [x] Received '" + in + "'");
         Request request = new Request();
         request.setContent(in);
         requestRepository.save(request);
-        */
+        
     }
 
     public static void main(String[] args) {
